24bb7e06990eca07f36fc11fe11cc620d706cb61
Subhadip Dey <sdey2307@gmail.com>
2025-07-10 09:50:55 +0530

    Create install-google-chrome-ubuntu-16.sh

---
+++ install-google-chrome-ubuntu-16.sh
@@ -0,0 +1,2 @@
+wget https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_109.0.5414.119-1_amd64.deb
+sudo dpkg -i google-chrome-stable_109.0.5414.119-1_amd64.deb
